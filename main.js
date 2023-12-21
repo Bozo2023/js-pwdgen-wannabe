27fed5df1f4c1bf06415965c54204da4e3c24420
Bozo2023 <ulrob1.0@email.com>
2023-12-21 16:19:11 +0100

    intro

---
+++ main.js
@@ -0,0 +1,5 @@
+
+let nome=prompt( 'Entra il nome');
+let cognome=prompt( 'Entra il cognome');
+let colorePreferito=prompt( 'Entra il intuo colore preferito');
+document.writeln(+nome+cognome+colorePreferito);
